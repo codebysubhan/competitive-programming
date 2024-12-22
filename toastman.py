c7232df370153fad5318db63247958d735e7d9be
codebysubhan <bsdsf22m030@pucit.edu.pk>
2024-12-22 17:10:39 +0500

    attempt 3

--- toastman.py
+++ toastman.py
@@ -1,16 +1,11 @@
+
+
 n = int(input())
 arr = list(map(int, input().split()))
-print(arr)
-arr.sort()
-def toast(tot,inp):
-    tot += sum(inp)
-    return (tot, inp)
-def apple(grp):
-    if len(grp)<=1:
-        return grp
-    mid = len(arr)//2
-    return (grp[:mid], grp[mid:])
 
-
-print(apple(arr))
-
+# out = sum(arr)
+if len(arr) == 1:
+    print(arr[0])
+else:
+    arr = [3*i for i in arr]
+    print(sum(arr))
